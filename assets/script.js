bf9322db8f06d81d843e2e7d913e29b507966db1
JawedBS <145364921+JawedBS@users.noreply.github.com>
2024-04-05 10:50:26 +0200

    retire slide en trop

--- assets/script.js
+++ assets/script.js
@@ -14,14 +14,6 @@ const slides = [
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Slide5"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Slide5"
 	}
 ]
 let position = 0
